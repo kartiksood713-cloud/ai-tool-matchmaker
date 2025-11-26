3d9dc9e5d9ea8648645128a3c0d1d6618304d7fb
kartiksood713-cloud <kartiksood713@gmail.com>
2025-11-26 17:34:02 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import ChatUI from "./components/ChatUI";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="chat-container">
+        <ChatUI />
+      </div>
+    </main>
+  );
+}
