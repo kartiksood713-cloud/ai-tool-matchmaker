b3128e158dafeca34b67ccee861c52093df374c8
kartiksood713-cloud <kartiksood713@gmail.com>
2025-11-27 14:34:00 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import ChatUI from "./components/ChatUI";
 
 export default function Page() {
   return (
-    <div style={{ width: "100%", height: "100vh" }}>
+    <main style={{ width: "100%", height: "100vh" }}>
       <ChatUI />
-    </div>
+    </main>
   );
 }
